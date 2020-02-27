ec77c00fe6ecb4928c79cd3accdc3b875e077999
erenhncr <erenhncr@gmail.com>
2020-02-27 18:34:22 +0300

    all functionalities added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../src/style.css';
+
 
 
 import Navbar from './components/navbar.component';
--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
@@ -16,10 +17,15 @@ export default class CreateExercise extends Component {
     }
 
     componentDidMount(){
-        this.setState({
-            users:['test user'],
-            username: 'test user'
-        });
+      axios.get('http://localhost:5000/users/')
+      .then(res => {
+        if(res.data.length > 0){
+          this.setState({
+            users: res.data.map(user => user.username),
+            username: res.data[0].username
+          });
+        }
+      });    
     }
 
     onChangeUsername = (e) => {
@@ -50,8 +56,11 @@ export default class CreateExercise extends Component {
         e.preventDefault();
         
         const exercise = this.state;
-        //{username,description,duration,date}
+
         console.log(exercise);
+
+        axios.post('http://localhost:5000/exercises/add',exercise)
+          .then(res => console.log(res.data));
 
         window.location='/';
     }
